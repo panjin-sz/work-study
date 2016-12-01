7dd3508fe6250dfd402593380da9c673708f9bea
潘进 <panjin@myhome.163.com>
2016-12-01 17:56:35 +0800

    modify single mode code

--- java-demo/src/main/java/com/panjin/design/singleton/SingletonFive.java
+++ java-demo/src/main/java/com/panjin/design/singleton/SingletonFive.java
@@ -10,12 +10,22 @@ package com.panjin.design.singleton;
  * @version $Id: SingletonFive.java 2016年6月25日 下午4:06:05 $
  */
 public class SingletonFive {
+    
+    private static boolean flag = false;
 
     private static class Holder {
         private static SingletonFive instance = new SingletonFive();
     }
 
     private SingletonFive() {
+        synchronized (SingletonFive.class) {
+            // 防止通过反射模式构造单列对象
+            if (flag == false) {
+                flag = !flag;
+            } else {
+                throw new RuntimeException("单列模式被侵犯");
+            }
+        }
     }
 
     public static SingletonFive getSingleton() {
